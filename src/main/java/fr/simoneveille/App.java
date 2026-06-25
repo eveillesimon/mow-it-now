7d9bb8b81920341ff3fd81a3ff579c3a8fd4dab1
Simon Eveillé <eveillesimon@gmail.com>
2026-06-25 11:30:28 +0200

    Project setup. Updated to JUnit 5.

---
+++ src/main/java/fr/simoneveille/App.java
@@ -0,0 +1,13 @@
+package fr.simoneveille;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fr/simoneveille/AppTest.java
@@ -0,0 +1,13 @@
+package fr.simoneveille;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void shouldReturnExpectedValue() {
+        assertEquals(2, 1 + 1);
+    }
+}
